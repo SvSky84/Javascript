f64067571e9cc12e43f99c31236024c6cdcfb057
Unknown <svsky84@gmail.com>
2017-10-16 15:14:42 +0200

    Exo 10 Objet

---
+++ js/objet.js
@@ -0,0 +1,28 @@
+/*
+var reference = new Object();
+
+reference.nom = 'HD';
+reference.pu = 2.2;
+reference.quantite = 10;
+reference.remise = 5;
+
+reference.ajoute10 = function(){
+	reference.quantite += 10;
+}
+
+*/
+
+/*
+function reference(nom, pu, quantite, remise){
+	this.nom = nom;
+	this.pu = pu;
+	this.quantite = quantite;
+	this.remise = remise;
+	this.ajoute10 = function(){
+		this.quantite += 10;
+	}
+}
+*/
+
+
+
